9661ca6f0e61d9c48de3ee130f3d522702c8a0f8
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2024-12-12 16:58:48 +0100

    Creo la carpeta hok de nuevo con use movies(me equivoqué de ubicación), modifico App.tsx y genero el dataMovies con la extensión

--- App.tsx
+++ App.tsx
@@ -1,23 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
-import MovieScreen from './src/screens/MovieScreen';
-import MovieScreen2 from './src/screens/MovieScreen2';
+//import HomeScreen from './src/screens/HomeScreen';
+//import MovieScreen from './src/screens/MovieScreen';
+//import MovieScreen2 from './src/screens/MovieScreen2';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Movies" component={MovieScreen} />
-        <Stack.Screen name="Movies2" component={MovieScreen2} />
-        
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
 }
 
 const styles = StyleSheet.create({
