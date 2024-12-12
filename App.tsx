fb2aa601ed83040fa31e77fef41f37fe61360555
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2024-12-12 16:33:49 +0100

    Utilizo (npx expo install -- --save-dev typescript @types/react) y creo el App.tsx

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import MovieScreen from './src/screens/MovieScreen';
+import MovieScreen2 from './src/screens/MovieScreen2';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Movies" component={MovieScreen} />
+        <Stack.Screen name="Movies2" component={MovieScreen2} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fcfafa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
